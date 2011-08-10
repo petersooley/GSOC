b69eaebaa1c6593e6b5b565343bb5fa4992ad1a7
Peter Soots <psoots@gmail.com>
2011-08-09 21:10:30 -0600

    Consolidating everything into one big app.

---
+++ geoFuncs.js
@@ -0,0 +1,323 @@
+/*
+ * Copyright © 2011 by Peter Soots
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *
+ * geoFuncs.js
+ *
+ * These are useful functions for working with OpenLayers to create an application
+ * for georeferencing.
+ *
+ */
+
+
+// A world file is
+// A: x scale
+// E: y scale (negative is the normative value)
+// C: Upperleft X coordinate
+// F: Upperleft Y coordinate
+// D & B: rotation...these are complicated, look at wikipedia
+function WorldFile(A, D, B, E, C, F) {
+	this.A = A;
+	this.D = D;
+	this.B = B;
+	this.E = E;
+	this.C = C;
+	this.F = F;
+	
+}
+WorldFile.prototype.display = function(div) {
+	document.getElementById(div).innerHTML = 
+		"<h3>World File (pixels)</h3>"+this.A+"<br>"+this.D+"<br>"+
+			this.B+"<br>"+this.E+"<br>"+this.C+"<br>"+this.F;
+}
+WorldFile.prototype.toData = function() {
+	return { "A": this.A, 
+				"D": this.D, 
+				"B": this.B, 
+				"E": this.E, 
+				"C": this.C, 
+				"F": this.F };
+}
+
+function Point(x, y) {
+	this.x = x;
+	this.y = y;
+}
+Point.prototype.getXComponent = function (PointObj) {
+	return PointObj.x - this.x;
+}
+Point.prototype.getYComponent = function (PointObj) {
+	return this.y - PointObj.y; // negative if downwards 
+}
+Point.prototype.moveXDirection = function (distance) {
+	this.x += distance;
+	return this.x;
+}
+Point.prototype.moveYDirection = function (distance) {
+	this.y += distance;
+	return this.y;
+}
+Point.prototype.clone = function () {
+	return new Point(this.x, this.y);
+}
+Point.prototype.toString = function() {
+	return this.x+", "+this.y;
+}
+
+function Image(url, width, height) {
+	this.url = url;
+	this.width = width;
+	this.height = height;
+}
+
+// Calculates a world file and returns a WorldFile object. 
+// The sub image is the one being altered to match the unaltered
+// base image. The calculation requires height and width of both 
+// images and two Point objects on each image: subA will be 
+// referenced to baseA and similarly, subB to baseB. These points
+// are in reference to a top-left origin where x and y are both 
+// positive.
+function writeWorldFile(subW, subH, subA, subB, baseW, baseH, baseA, baseB) {
+	//l("subW: "+subW); 
+	//l("subH: "+subH);
+	//l("subA: "+subA);
+	//l("subB: "+subB);
+	//l("baseW: "+baseW);
+	//l("baseH: "+baseH);
+	//l("baseA: "+baseA);
+	//l("baseB: "+baseB);
+	 
+	// Scaling
+	///////////////////////////////////////////////////////////////////////////////
+	// To scale the image we just need to calculate the X and Y components between
+	// point A and point B in both images. To figure out how much to scale the sub
+	// image we just figure out the right ratio between the base image's components
+	// and the sub image's components.
+	var subXComponent = subA.getXComponent(subB);
+	var subYComponent = subA.getYComponent(subB);
+	var baseXComponent = baseA.getXComponent(baseB);
+	var baseYComponent = baseA.getYComponent(baseB);
+	//l("subXComponent: "+subXComponent);
+	//l("subYComponent: "+subYComponent);
+	//l("baseXComponent: "+baseXComponent);
+	//l("baseYComponent: "+baseYComponent);
+	
+	// Calculate the ratio. NOTE: Our math uses the Cartesian plane, but internet
+	// images don't, so the yScale is opposite from what you'd think it should be.
+	var xScale = baseXComponent / subXComponent;
+	var yScale = baseYComponent / subYComponent * -1;
+	//l("xScale: "+xScale);
+	//l("yScale: "+yScale);
+	
+	
+	// Translation 
+	////////////////////////////////////////////////////////////////////////////////////
+	// Get the X and Y of SubA. Once we place the origin of the sub image onto BaseA
+	// we'll need to "move back" so that SubA is on BaseA. But now that we're working in 
+	// the base image's space, we need to use its scaling.
+	
+	var origin = new Point(0,0);
+	//l("x: "+origin.getXComponent(subA));
+	//l("y: "+origin.getYComponent(subA));
+	var moveX = origin.getXComponent(subA) * xScale;
+	var moveY = origin.getYComponent(subA) * yScale; 
+	// No matter what, we need to move up and to the left. 
+	if(moveX < 0) moveX *= -1;
+	if(moveY > 0) moveY *= -1;
+	//l("moveX: "+moveX);
+	//l("moveY: "+moveY);
+	 
+	// If we place the sub image so that it's origin is on BaseA, we then need to counter
+	// that movement by moving SubA back to its origin. So, get the X and Y of BaseA and
+	// subtract the X and Y of SubA (in the base image's scaling).
+	var upperleftX = origin.getXComponent(baseA) - moveX;
+	var upperleftY = origin.getYComponent(baseA) - moveY;
+	//l("upperleftX: "+upperleftX);
+	//l("upperleftY: "+upperleftY);
+	
+	/* Skipping rotation for now
+	
+	// Rotation
+	////////////////////////////////////////////////////////////////////////// 
+	// Find the angles that the AB line makes with the x-axis. By using tangent
+	// we'll only get values from pi/2 to -pi/2. This is exactly what we want.
+	// We don't need to carry on the calculation to find out the real angle,
+	// because the image will be flipped in the transformation according to 
+	// the positive or negative sign of the xScale and yScale values that we
+	// already calculated. It makes the math really simple.
+	var subAngle = Math.atan(subYComponent / subXComponent);
+	var baseAngle = Math.atan(baseYComponent / baseXComponent);
+	l("subAngle (degrees): "+(subAngle * (180 / Math.PI)));
+	l("baseAngle (degrees): "+(baseAngle*(180 / Math.PI)));
+	
+	
+	// The angle of rotation is how much we need to rotate the sub image
+	// to fit on the base image. This will help us determine lines B and D
+	// of the world file. We can figure out the rotation by doing a simple
+	// subtraction because all the hard work of angle geometry is solved 
+	// when the xScale and yScale flip the image leaving us with just one
+	// quadrant to work in. However, we have to be careful that negative
+	// angles are subtracted correctly.
+	
+	var angleOfRotation;
+	if((xScale < 0 || yScale < 0) && !(xScale < 0 && yScale < 0))
+		angleOfRotation = baseAngle + subAngle; // one of the angles is negative
+	else
+		angleOfRotation = baseAngle - subAngle; // the angles are either both pos. or both neg.
+	l("angleOfRotation (degrees): "+(angleOfRotation * (180 / Math.PI)));
+	
+	
+	// Now that we're doing rotation, we need to rethink the xScale and yScale 
+	// variables, lines A and E. So far, these variables have simply represented
+	// the pixel width and height.
+	var pixelWidth = xScale;
+	var pixelHeight = yScale;
+	l("pixelWidth: "+pixelWidth);
+	l("pixelHeight: "+pixelHeight);
+	
+	// But with rotation, pixel width and height aren't needed in the world file. 
+	// Instead lines A and E represent the x and y components of the rotated pixel, 
+	// respectively.
+	xScale = pixelHeight * Math.cos(angleOfRotation);
+	yScale = pixelWidth * Math.cos(angleOfRotation);
+	l("xScale: "+xScale);
+	l("yScale: "+yScale);
+	
+	// Lastly, the xSkew and ySkew variables in the world file, lines B and D, 
+	// are the other components of the rotated pixel. ySkew is the y component
+	// of the pixelWidth and the xSkew is the x component of the pixelHeight.
+	var ySkew = pixelWidth * Math.sin(angleOfRotation);
+	var xSkew = pixelHeight * Math.sin(angleOfRotation);
+	l("xSkew: "+xSkew);
+	l("ySkew: "+ySkew);
+	
+	*/
+	var xSkew = 0;
+	var ySkew = 0;
+
+	return new WorldFile(xScale, ySkew, xSkew, yScale, upperleftX, upperleftY);
+	
+}
+
+// Turns an imageObject into a layer (used by makeMapFromImage)
+function makeImageLayer(name, imageObject) {
+	var w = imageObject.width;
+	var h = imageObject.height;
+	
+	// In Openlayers coordinates, the origin is in the middle of the
+	// image. So, we just have to plan our boundaries accordingly, by
+	// dividing our heights and widths by 2 to find the centers.	
+	var bounds = new OpenLayers.Bounds(-(w / 2), -(h / 2), (w / 2), (h / 2));
+	var size = new OpenLayers.Size(w, h);
+	
+	
+	// We need to determine the max resolution, because small images won't be 
+	// zoomed in enough and large images will be zoomed in too far. This 
+	// calculation was just a guess, but it does the job perfectly.
+	var area = h * w;
+	var res = Math.log(area) * Math.LOG10E; // calculates base 10 of area
+	
+	var opts = {
+		numZoomLevels	: 10, // arbitrary
+		maxResolution	: res   // one pixel per one map unit
+	};
+	return new OpenLayers.Layer.Image(name, imageObject.url, bounds, size, opts);
+}
+
+// Turns an imageObject into a map
+function makeMapFromImage(div, name, imageObject) {
+	var map = new OpenLayers.Map(div);
+	map.addLayer( makeImageLayer(name, imageObject) );
+	map.zoomToMaxExtent();
+	return map;
+}
+
+// Adds a vector layer to mapObject as an interface that allows
+// the user to add control points to the image. This is where
+// we style the control points.
+function addControlPointsLayer(mapObject, layerName) {
+	var vl = new OpenLayers.Layer.Vector(layerName);
+	mapObject.addLayer(vl);
+	
+	var count = (function() { 
+		var i = 0; 
+		return function() { 
+			return ++i; 
+		};
+	})();
+	
+	// Increment the label each time a point is added
+	vl.preFeatureInsert = function(feature) { 
+		var string = "";
+		feature.attributes = {
+			label	: count()
+		};
+	}
+	
+	
+	// Add styles to the vector layer (to display the label)
+	// These settings are all default (but if you change one setting, they
+	// all go away, so you have to repeat them like I am doing here.)
+	var vs = new OpenLayers.Style({
+		'label'			: '${label}', // <--This is the whole point of the Style object
+		'fillColor'		: '#ee9900',
+		'fillOpacity'	: .6,
+		'strokeWidth'	: 2,
+		'strokeColor'	: '#ee9900',
+		'strokeOpacity': .7, 
+		'pointRadius'	: 6,
+		'labelXOffset'	: -10,
+		'labelYOffset'	: -10
+	});
+	var vsm = new OpenLayers.StyleMap({
+		'default'		: vs
+	});
+	
+	vl.styleMap = vsm;
+	
+	// Add the DrawPoints Control
+	var control = new OpenLayers.Control.DrawFeature(vl, OpenLayers.Handler.Point);
+	mapObject.addControl(control);
+	control.activate();
+
+	
+	return vl;
+}
+
+// Capture user's clicks to the vectorLayer and save them in the pointArray
+function collectPoints(vectorLayer, pointArray, outputDiv, imageObj) {
+	// Register for featureadded events
+	vectorLayer.events.register('featureadded', this, function(event) { 
+		
+		// Change the points so the origin is at the top left rather than at the center
+		var x = event.feature.geometry.x + (imageObj.width / 2); 
+		var y = (imageObj.height / 2) - event.feature.geometry.y;
+		
+		// add point to the array
+		var p = new Point(x, y);
+		pointArray[pointArray.length] = p;
+		
+		// Temporarily display the points
+		var string = "";
+		for(var i = 0; i < pointArray.length; ++i) {
+			string = string + "<p>Point #"+(i+1)+"	x: "+pointArray[i].x+"	y: "+pointArray[i].y +"</p>";
+		}
+		document.getElementById(outputDiv).innerHTML = string; 
+	});
+}
--- index.php
+++ index.php
@@ -23,45 +23,304 @@
  *
  * index.php
  *
- * This is the entry to our application. We just need to get some images from
- * the user and pass it on to main.php.
+ * This is our entire application. Details to come...
  * 
  */
  
- session_start(); 
- 
- $_SESSION['subImages'] = array();
- $_SESSION['subWorldFiles'] = array();
- $_SESSION['subCount'] = 0;
+session_start(); 
 
- 
- ?>
+ini_set('display_errors',1); 
+error_reporting(E_ALL);
+?>
+<!-- This is the html that is displayed every time we load this page. -->
 <!doctype html public "-//w3c//dtd html 4.0 transitional//en"> 
 <html> 
 <head> 
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
-	<title>Google Summer of Code Sandbox</title> 
-	<!-- Sytlesheets -->
-	<link rel="stylesheet" href="styles/style.css" type="text/css" /> 
-   <link rel="stylesheet" href="styles/examples.css" type="text/css" /> 
+	<title>Georeferencing Images</title> 
+	
+<?php if(isset($_SESSION['needOpenLayers'])) { ?>
+			<script type="text/javascript" src="imports.js"></script> 
+			<script src="OpenLayers/lib/OpenLayers.js"></script>	
+			
+			<style type="text/css">
+				.smallmap {
+    				width: 512px;
+    				height: 256px;
+    				border: 1px solid #ccc;
+				}
+				#sidepanel {
+ 					float: left;
+ 					margin-left: 10px;
+ 					width: 460px;
+ 				}
+ 				#originals {
+ 					float: left;
+ 				}
+ 				#genButton {
+					width: 460px;
+					text-align: center;
+ 				}
+ 				.points {
+ 					height: 170px;
+ 					overflow: auto;
+ 				}
+			</style>
+<?php } ?>
+
 </head>
 <body>
 
-<h1>Google Summer of Code Sandbox</h1>
-<a href="https://github.com/psoots/GSOC">Source Code on Github</a>
-<form action="main.php" method="post" enctype="multipart/form-data">
-<label for="inputBaseImage">Base Image (not changed)</label>
-<input type="file" name="baseImage" id="inputBaseImage"/>
-<label for="inputSubImage">Sub Image</label>
-<input type="file" name="subImage" id="inputSubImage"/>
-<input type="submit" value="submit" />
-</form>
-<div id="error">
+<h1>Georeferencing Images</h1>
+
 <?php
-if(isset($_GET['error']))
-	echo("There was an error uploading your files. Try again.");
+
+
+// This is our state machine that takes the user through the application.
+// The states are changed as the user clicks on forms (or buttons that
+// eventually send forms via JavaScript) which contain hidden input fields
+// that request the next state. 
+if(!isset($_POST['state']))
+	goto getBase;
+switch($_POST['state']) {
+	case 'gotBase':		goto saveBase;
+	case 'getSub':			goto getSub;
+	case 'gotSub':			goto saveSub;
+	case 'gotWorldFile':	goto alterImage;
+	case 'done':			goto done;
+}
+
+
+// This is a struct not a class. It just makes passing data WAY easier.
+class Image {
+	public $height;
+	public $width;
+	public $url;
+	public $worldFile;
+}
 ?>
 
-</div>
 
+
+<?php ///////////////////////////////////////////////////////////////
+getBase: 
+// This is the beginning state where we get the base image
+// from the user. This state only happens once. 
+
+// Iniatilize some variables since this is the very beginning of the session.
+
+// Variables for keeping track of possibly many different sub images.
+$_SESSION['subImages'] = array();
+$_SESSION['subCount'] = 0;
+$_SESSION['baseImage'] = new Image();
+
+// This is the location of our temporary folder to store images and files.
+$_SESSION['dataDir'] = "data/";	
+$_SESSION['mapFile'] = $_SESSION['dataDir']."mapfile.map";
+
+?>
+
+<p>
+The purpose of this application is to spatially relate images together
+in a way that can be displayed easily on the web. Originially, this 
+project will be used to display thin-section images collected with a 
+microscope "on top of" higher resolution images obtained by 
+electromicroscopy, but this application is general enough to be used
+for other purposes too. The final method for displaying all of the images
+together uses a zoomable, map-like interface that gives the viewer 1) 
+a way to spatially understand how the lower resolution images relate
+to the higher resolution images and 2) easy access to viewing the higher
+resolution images within the proper spatial context.
+</p>
+</p>
+The technologies used are:
+	<ul>
+		<li>PHP,</li>
+		<li>JavaScript,</li>
+		<li>MapServer, and</li>
+		<li>OpenLayers.</li>
+	</ul>
+</p>
+<p>
+To get started, you need a base image (typically of the highest resolution).
+This will not be <i>changed</i>. That is, all other images will be scaled and
+translated onto this one image. Later, you will add more (lower resolution) 
+images that you will carefully place onto this one base image.
+</p>
+<form action="index.php" method="post" enctype="multipart/form-data">
+	<label for="baseImage">Base Image (not changed)</label>
+	<input type="file" name="baseImage" id="baseImage"/>
+	<input type="hidden" name="state" id="state" value="gotBase" />
+	<input type="submit" value="submit" />
+</form>
+<?php goto endPage; ?>
+
+<?php ///////////////////////////////////////////////////////////////
+saveBase: 
+// Save the base image to disk so that we can use it throughout the 
+// application. This state only happens once. 
+
+// Get the details about the base image. We'll use these details later in the javascript.
+list($w, $h, $type, $ignored) = getimagesize($_FILES['baseImage']['tmp_name']);
+$baseFile = $_SESSION['dataDir']."base";
+$base = new Image();
+$base->height = $h;
+$base->width = $w;
+	
+// Store the image url in a session variable and save the image to the data directory.
+switch($type) {
+	case IMAGETYPE_GIF:
+		$base->url = $baseFile.".gif";
+		$base->worldfile = $baseFile.".gfw";
+		imagegif(imagecreatefromgif($_FILES['baseImage']['tmp_name']), $base->url);
+		break;
+	case IMAGETYPE_JPEG:
+		$base->url = $baseFile.".jpg";
+		$base->worldfile = $baseFile.".jgw";
+		imagejpeg(imagecreatefromjpeg($_FILES['baseImage']['tmp_name']), $base->url);	
+		break;
+	case IMAGETYPE_PNG:
+		$base->url = $baseFile.".png";
+		$base->worldfile = $baseFile.".pgw";		
+		imagepng(imagecreatefrompng($_FILES['baseImage']['tmp_name']), $base->url);
+		break;
+}
+
+$_SESSION['baseImage'] = $base;
+
+
+/////////////////////////////////////////////////////////////////////
+getSub: 
+// We get a sub image from the user. We re-enter this state whenever a 
+// user wants to add more sub images on top of the base image. 
+
+?>
+
+<p>
+Select an image to place onto the base image.
+</p>
+<form action="index.php" method="post" enctype="multipart/form-data">
+	<label for="subImage">Sub image</label>
+	<input type="file" name="subImage" id="subImage"/>
+	<input type="hidden" name="state" id="state" value="gotSub" />
+	<input type="submit" value="submit" />
+</form>
+<?php 
+// The next reload will eventually use the OpenLayers JavaScript library. So
+// we need to make sure it gets loaded.
+$_SESSION['needOpenLayers'] = true;
+goto endPage; 
+?>
+
+<?php ///////////////////////////////////////////////////////////////
+saveSub:
+// Save the sub image that the user just uploaded. Also, we maintain
+// the session variables that keep track of how many sub images the
+// user has uploaded.
+
+
+// Get the details about the sub image. 
+list($w, $h, $type, $ignored) = getimagesize($_FILES['subImage']['tmp_name']);
+$_SESSION['subName'] = "sub";
+$subFile = $_SESSION['dataDir'].$_SESSION['subName'].$_SESSION['subCount'];
+$sub = new Image();
+$sub->height = $h;
+$sub->width = $w;
+
+// Store the image url in a session variable and save the image to the images directory.
+switch($type) {
+	case IMAGETYPE_GIF:
+		$sub->url = $subFile.".gif";
+		$sub->worldfile = $subFile.".gwf";
+		imagegif(imagecreatefromgif($_FILES['subImage']['tmp_name']), $sub->url);
+		break;
+	case IMAGETYPE_JPEG:
+		$sub->url = $subFile.".jpg";
+		$sub->worldfile = $subFile.".jgw";
+		imagejpeg(imagecreatefromjpeg($_FILES['subImage']['tmp_name']), $sub->url);
+		break;
+	case IMAGETYPE_PNG:
+		$sub->url = $subFile.".png";
+		$sub->worldfile = $subFile.".pgw";
+		imagepng(imagecreatefrompng($_FILES['subImage']['tmp_name']), $sub->url);	
+}
+
+// Save data in Session
+$_SESSION['subCount'] = $_SESSION['subCount'] + 1;
+array_push($_SESSION['subImages'], $sub);
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////
+// collectPoints: 
+// We display the base image (with any previously altered sub images)
+// and we display the new sub image to be altered. We use OpenLayers 
+// to give the user an interface to add control points to the base
+// and sub images. Then we use the control points to calculate a 
+// world file. 
+
+
+
+?>
+
+
+<?php goto endPage; ?>
+
+
+
+
+
+
+
+<?php ///////////////////////////////////////////////////////////////
+alterImage: 
+// This is where we create a mapfile and then use OpenLayers to bring
+// the sub images together with the base image. ?>
+
+
+
+<?php goto endPage; ?>
+
+
+
+
+
+
+<?php ///////////////////////////////////////////////////////////////
+done: 
+// At this point, the user has added all of his or her sub images to
+// the base image. Now we generate a zipfile that contains everything
+// the user needs to display all of the images together including the
+// images that the user supplied (but renamed) and the world files, 
+// mapfiles and the final html file. 
+
+
+
+?>
+
+
+<?php goto endPage; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php ///////////////////////////////////////////////////////////////
+endPage: 
+?>
+<a href="https://github.com/psoots/GSOC">Source Code on Github</a>
 </body>
+</html>
